5ab574bebfad6133c91726f26460c85ce255c366
Shivek Khurana <khuranashivek@gmail.com>
2013-11-05 20:16:43 +0530

    Task are awesome. The logic to kill them should be in built

--- js/node/truecrawler/actions/console.js
+++ js/node/truecrawler/actions/console.js
@@ -12,6 +12,11 @@ exports.action = {
   matchExtensionMimeType: false,
   version: 1.0,
   run: function(api, connection, next){
+    task = new api.task({
+      name:"scraper",
+      params:{name:"shivek"}
+    });
+    task.enqueue();
     next(connection, true);
   }
 };
