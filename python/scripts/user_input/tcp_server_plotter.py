ced72e2a0c7f1d6743f6b9681550c1ad0b86c01e
Shivek Khurana <khuranashivek@gmail.com>
2013-12-13 13:14:03 +0530

    Clearing extra stuff

--- python/scripts/user_input/tcp_server_plotter.py
+++ python/scripts/user_input/tcp_server_plotter.py
@@ -2,83 +2,78 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-####################################
-# Sample Receiver Script
-# [Usage]
-#	python receiver.py
-#	python receiver.py > data.csv
-# [Data Format]
-#	id,time,x,y,z
-# [Exaple]
-#	1,118.533,-0.398,-0.199,-0.978
-####################################
+import socket 
 
-import os
-import math
-import time
-import SocketServer
+host = '' 
+port = 50000 
+backlog = 5 
+size = 1024 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+s.bind((host,port)) 
+s.listen(backlog) 
 
-PORTNO = 10552
-class handler(SocketServer.DatagramRequestHandler):
-	plt.ion() #set interactive
-	plt.show()
+plt.ion() #set interactive
+plt.show()
 
-	(ax_list, ay_list, az_list) = ([],[],[])
-	(ax_factor, ay_factor, az_factor) = (1.0,1.0,1.0)
-	
-	t_step = 0.3
-	t = 0
-	t_list = []
+(ax_list, ay_list, az_list) = ([],[],[])
+(ax_factor, ay_factor, az_factor) = (20.0,20.0,20.0)
 
-	
-	fig=plt.figure(num=None, figsize=(13, 9), dpi=75, facecolor='w', edgecolor='k')
-	(thresh_lower, thresh_upper) = (0,0)
-	x_min = 0
-	x_max = t_step*5
-	y_min = -2
-	y_max = 2
-	plt.axis([x_min,x_max,y_min,y_max])
+t_step = 0.3
+t = 0
+t_list = []
 
-	def handle(self):
-		data = self.rfile.readline().rstrip().split(',')
+fig=plt.figure(num=None, figsize=(13, 9), dpi=75, facecolor='w', edgecolor='k')
+(thresh_lower, thresh_upper) = (0,0)
+x_min = 0
+x_max = t_step*5
+y_min = -2
+y_max = 2
+plt.axis([x_min,x_max,y_min,y_max])
 
-		if len(data) > 0:
-			try:
-				(ax, ay, az) = (float(data[1])*self.ax_factor, float(data[2])*self.ay_factor, float(data[3])*self.az_factor)
-			except ValueError:
-				(ax, ay, az) = (0,0,0) #fix headings transfer
-			self.ax_list.append(ax)
-			#self.ay_list.append(ay)
-			#self.az_list.append(az)
-			self.t_list.append(self.t)
+def main():	
+	try:
+		while True: #poll
+			client, address = s.accept() 
+			data = client.recv(size).split(',')
+			if len(data) > 0:
+				try:
+					(ax, ay) = (float(data[3])*ax_factor, float(data[4])*ay_factor)
+				except ValueError:
+					(ax,ay) = (0,0) #fix headings transfer
+				ax_list.append(ax)
+				#ay_list.append(ay)
+				#az_list.append(float(data[5])*az_factor)
+				t_list.append(t)
 
-			plt.plot(self.t_list, self.ax_list, color="red", linewidth=1.0, linestyle="-", label="Ax")
-			#plt.plot(self.t_list, self.ay_list, color="green", linewidth=1.0, linestyle="-",  label="Ay")
-			#plt.plot(self.t_list, self.az_list, color="blue", linewidth=1.0, linestyle="-", label="Az")
+				plt.plot(t_list, ax_list, color="red", linewidth=1.0, linestyle="-", label="Ax")
+				#plt.plot(t_list, ay_list, color="green", linewidth=1.0, linestyle="-",  label="Ay")
+				#plt.plot(t_list, az_list, color="blue", linewidth=1.0, linestyle="-", label="Az")
 
-			self.t+=self.t_step
+				t+=t_step
 
-			if self.t > self.x_max:
-				#move axis forward
-				self.x_min += self.t_step
-				self.x_max += self.t_step
-				plt.axis([self.x_min, self.x_max, self.y_min, self.y_max])		
+				if t > x_max:
+					#move axis forward
+					x_min += t_step
+					x_max += t_step
+					plt.axis([x_min,x_max,y_min,y_max])		
 
-			if ax < self.thresh_lower:
-				plt.axhline(y=self.thresh_lower, c='w', ls='-')
-				self.thresh_lower = ax
-				plt.axhline(y=self.thresh_lower, c='k', ls='--')		
+				if ax < thresh_lower:
+					plt.axhline(y=thresh_lower, c='w', ls='-')
+					thresh_lower = ax
+					plt.axhline(y=thresh_lower, c='k', ls='--')		
 
-			if ax > self.thresh_upper:
-				plt.axhline(y=self.thresh_upper, c='w', ls='-')
-				self.thresh_upper = ax
-				plt.axhline(y=self.thresh_upper, c='k', ls='--')		
+				if ax > thresh_upper:
+					plt.axhline(y=thresh_upper, c='w', ls='-')
+					thresh_upper = ax
+					plt.axhline(y=thresh_upper, c='k', ls='--')		
 
-			plt.draw()
-			print self.t,data #dump on terminal
+				plt.draw()
+				print t,data[3] #dump on terminal
 
-
-
-s = SocketServer.UDPServer(('',PORTNO), handler)
-print "Awaiting UDP messages on port %d" % PORTNO
-s.serve_forever()
+	except Exception as inst:
+		print sys.exc_info()[0]
+		print data
+		raise
+		
+if __name__ == "__main__":
+	main()
