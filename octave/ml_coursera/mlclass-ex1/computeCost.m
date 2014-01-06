43706d2ccf428625c4c7fe08b1b03d3e91f72478
Shivek Khurana <khuranashivek@gmail.com>
2014-01-06 20:44:49 +0530

    Running gradient descent

--- octave/ml_coursera/mlclass-ex1/computeCost.m
+++ octave/ml_coursera/mlclass-ex1/computeCost.m
@@ -13,10 +13,11 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+	for i=1:m
+		J = J + (1/(2*m))*((theta(2)*X(i,2) + theta(1) - y(i))^2);
+	end
 
 
-
-
-% =========================================================================
+% ===============================================================
 
 end
--- octave/ml_coursera/mlclass-ex1/gradientDescent.m
+++ octave/ml_coursera/mlclass-ex1/gradientDescent.m
@@ -1,33 +1,29 @@
 function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
-%GRADIENTDESCENT Performs gradient descent to learn theta
-%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
-%   taking num_iters gradient steps with learning rate alpha
+    %GRADIENTDESCENT Performs gradient descent to learn theta
+    %   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
+    %   taking num_iters gradient steps with learning rate alpha
 
-% Initialize some useful values
-m = length(y); % number of training examples
-J_history = zeros(num_iters, 1);
-
-for iter = 1:num_iters
-
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCost) and gradient here.
-    %
+    % Initialize some useful values
+    m = length(y); % number of training examples
+    J_history = zeros(num_iters, 1);
+    n = length(theta);% number of features
 
 
+    for iter = 1:num_iters %iter num_iter times
+        temp_theta = theta; %update temp_theta for simultaneous update
+        for i=1:n %iter over all thetas
+            hyposthesis_theta = theta*X();
 
 
+        end
 
+        % Save the cost J in every iteration  
+        % cost w.r.t computed theta
+        J_history(iter) = computeCost(X, y, theta); 
 
-
-    % ============================================================
-
-    % Save the cost J in every iteration    
-    J_history(iter) = computeCost(X, y, theta);
-
-end
+        
+        disp(sprintf('Iteration = %d\nTheta1 = %f , Theta2 = %f',iter,theta(1), theta(2)))
+        disp(sprintf('Cost J = %f\n\n',J_history(iter)))
+    end
 
 end
