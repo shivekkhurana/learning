e88472c7529a8d25f16b92698d99d631f2df8a6d
Shivek Khurana <khuranashivek@gmail.com>
2014-01-09 09:00:56 +0530

    Gradient descent running well locally, but test server not accepting stuff as I changed the structure of the program. Normal Equation giving different theta value.

--- octave/ml_coursera/mlclass-ex1/featureNormalize.m
+++ octave/ml_coursera/mlclass-ex1/featureNormalize.m
@@ -1,42 +1,19 @@
 function [X_norm, mu, sigma] = featureNormalize(X)
-%FEATURENORMALIZE Normalizes the features in X 
-%   FEATURENORMALIZE(X) returns a normalized version of X where
-%   the mean value of each feature is 0 and the standard deviation
-%   is 1. This is often a good preprocessing step to do when
-%   working with learning algorithms.
+	%FEATURENORMALIZE Normalizes the features in X 
+	%   FEATURENORMALIZE(X) returns a normalized version of X where
+	%   the mean value of each feature is 0 and the standard deviation
+	%   is 1. This is often a good preprocessing step to do when
+	%   working with learning algorithms.
 
-% You need to set these values correctly
-X_norm = X;
+	num_features = size(X, 2);
 
-	for feature=1:size(X,2)
-		mu = zeros(1, size(X, 2));
-		sigma = zeros(1, size(X, 2));
-		X_instant = X(:,feature);
-		mean_X_instant = mean(X_instant);
-		range_X_instant = max(X_instant) - min(X_instant);
+	for feature=1:num_features%for each column, i.e feature
+		mu = mean(X(:, feature));
+		sigma = svd(X(:, feature));
 
-		for i=1:length(mu)
-			mu(i) = (mu(i) - mean_X_instant)/range_X_instant;
+		for m=1:length(X(:,feature))
+			X(m, feature) = (X(m, feature) - mu)/sigma;
 		end
-
-		% ====================== YOUR CODE HERE ======================
-		% Instructions: First, for each feature dimension, compute the mean
-		%               of the feature and subtract it from the dataset,
-		%               storing the mean value in mu. Next, compute the 
-		%               standard deviation of each feature and divide
-		%               each feature by it's standard deviation, storing
-		%               the standard deviation in sigma. 
-		%
-		%               Note that X is a matrix where each column is a 
-		%               feature and each row is an example. You need 
-		%               to perform the normalization separately for 
-		%               each feature. 
-		%
-		% Hint: You might find the 'mean' and 'std' functions useful.
-		%       
-
-		X(:,feature) = mu;
-
-		% ============================================================
 	end
+	X_norm = X;
 end
--- octave/ml_coursera/mlclass-ex1/gradientDescentMulti.m
+++ octave/ml_coursera/mlclass-ex1/gradientDescentMulti.m
@@ -3,35 +3,29 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 %   theta = GRADIENTDESCENTMULTI(x, y, theta, alpha, num_iters) updates theta by
 %   taking num_iters gradient steps with learning rate alpha
 
-% Initialize some useful values
-m = length(y); % number of training examples
-J_history = zeros(num_iters, 1);
+    % Initialize some useful values
+    m = length(y); % number of training examples
+    J_history = zeros(num_iters, 1);
+    n = length(theta);
 
-for iter = 1:num_iters
+    for iter = 1:num_iters %iter num_iter times
+        temp_theta = theta; %update temp_theta for simultaneous update
+        for i=1:n %iter over all thetas
+            descent = 0;
+            for j=1:m
+                descent = descent + (theta(1)*X(j,1) + theta(2)*X(j,2) - y(j))*X(j,i); 
+            end
+            temp_theta(i) = temp_theta(i) - (alpha/m)*descent;
 
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCostMulti) and gradient here.
-    %
+        end
+        theta = temp_theta;
+        % Save the cost J in every iteration  
+        % cost w.r.t computed theta
+        J_history(iter) = computeCost(X, y, theta); 
 
-
-
-
-
-
-
-
-
-
-
-    % ============================================================
-
-    % Save the cost J in every iteration    
-    J_history(iter) = computeCostMulti(X, y, theta);
-
-end
+        
+        disp(sprintf('Iteration = %d\nTheta1 = %f , Theta2 = %f',iter,theta(1), theta(2)))
+        disp(sprintf('Cost J = %f\n\n',J_history(iter)))
+    end
 
 end
