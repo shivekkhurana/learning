1388fa4775c414f91eed008a386b994e52b6a3ac
Shivek Khurana <shivekkhurana@Apples-iMac-3.local>
2014-01-24 12:08:56 +0530

    Removed habitBuilder

---
+++ objective-c/TodoList/TodoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TodoList
+//
+//  Created by Shivek Khurana on 24/01/14.
+//  Copyright (c) 2014 Shivek Khurana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ objective-c/TodoList/TodoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TodoList
+//
+//  Created by Shivek Khurana on 24/01/14.
+//  Copyright (c) 2014 Shivek Khurana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
