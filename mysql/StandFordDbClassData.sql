fdc03ec819c84c07af0a8e5271415e6f9e474312
Shivek Khurana <khuranashivek@gmail.com>
2014-02-26 14:18:57 +0530

    Irene had sID=876 not 879. fixed

--- mysql/StandFordDbClassData.sql
+++ mysql/StandFordDbClassData.sql
@@ -36,7 +36,7 @@ insert into Student values(567, "Edward", 2.9, 2000);
 insert into Student values(678, "Fay", 3.8, 200);
 insert into Student values(789, "Gary", 3.4, 800);
 insert into Student values(987, "Helen", 3.7, 800);
-insert into Student values(879, "Irene", 3.9, 400);
+insert into Student values(876, "Irene", 3.9, 400);
 insert into Student values(765, "Jay", 2.9, 1500);
 insert into Student values(654, "Amy", 3.9, 1000);
 insert into Student values(543, "Craig", 3.4, 2000);
