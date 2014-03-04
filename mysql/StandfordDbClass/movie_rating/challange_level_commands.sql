30a511cd4725431df68db07d72fa54836cf86436
Shivek Khurana <khuranashivek@gmail.com>
2014-03-04 10:32:13 +0530

    Movie eating challange level done

--- mysql/StandfordDbClass/movie_rating/challange_level_commands.sql
+++ mysql/StandfordDbClass/movie_rating/challange_level_commands.sql
@@ -35,11 +35,19 @@ order by director, title;
 #and average rating. (Hint: This query is more difficult to write in SQLite 
 #than other systems; you might think of it as finding the highest average rating
 #and then choosing the movie(s) with that average rating.) 
+select title, avg_rating from 
+(select mID, avg(stars) as avg_rating from Rating group by mID) R 
+join 
+(select mID, title from Movie) M using(mID)
+where avg_rating >= (select max(avg_rating) from 
+	(select avg(stars) as avg_rating from Rating group by mID) R2 );
 
-select mID, avg(stars) as avg_rating from Rating group by mID;
 
-#>this query solves the exam but is not well formed
-select title, avg_rating from Movie join (select mID, avg(stars) as avg_rating from Rating 
-group by mID 
-order by avg_rating desc
-limit 1) X using(mID);
+#next question is similar to the above one
+
+#For each director, return the director's name together with the title(s) 
+#of the movie(s) they directed that received the highest rating among all 
+#of their movies, and the value of that rating. Ignore movies whose director is NULL.
+select director, title, max(stars) from Movie join Rating 
+using(mID) where director is not null
+group by director;
