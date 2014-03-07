75fba2012455bbd9f31d973e772b1b09ef246941
Shivek Khurana <khuranashivek@gmail.com>
2014-03-07 12:19:22 +0530

    This one query addition deserved a commit pf its own

--- mysql/StandfordDbClass/social_network/challange_level_commands.sql
+++ mysql/StandfordDbClass/social_network/challange_level_commands.sql
@@ -10,8 +10,20 @@ order by grade, name;
 #For each student A who likes a student B where the two are not friends, 
 #find if they have a friend C in common (who can introduce them!). For 
 #all such trios, return the name and grade of A, B, and C. 
-
-#select all people who like someone else but aren't friends
-select Likes.* from Likes left join Friend 
-on Friend.ID1 = Likes.ID1 and Friend.ID2 = Likes.ID2
-where Friend.ID1 is null and Friend.ID2 is null;
+select H1.name, H1.grade, H2.name, H2.grade, H3.name, H3.grade from 
+(
+	select liker, liked, likers_friend from Friend 
+	join 
+	(
+		select Friend.ID2 as likers_friend, liker, liked from Friend 
+		join 
+		(
+			select Likes.ID1 as liker, Likes.ID2 as liked from Likes left join Friend 
+			on Friend.ID1 = Likes.ID1 and Friend.ID2 = Likes.ID2
+			where Friend.ID1 is null and Friend.ID2 is null
+		) LL 
+		on LL.liker = Friend.ID1
+	) LFLL
+	on LFLL.likers_friend = Friend.ID1 and LFLL.liked = Friend.ID2
+) M, Highschooler H1, Highschooler H2, Highschooler H3
+where H1.ID = liker and H2.ID = liked and H3.ID = likers_friend;
